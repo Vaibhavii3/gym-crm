5c3deb911bde1a4616ebdf1407bd05a79aa3278d
Vaibhavi <vaibhavichaudhari304@gmail.com>
2025-05-07 22:28:08 +0530

    backend

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -26,23 +26,6 @@ export const registerAdmin = async (req, res) => {
     }
 };
 
-
-// export const loginAdmin = async (req, res) => {
-//     const { email, password } = req.body;
-//     const admin = await Admin.findOne({ email });
-
-//     if (admin && (await admin.matchPassword(password))) {
-//         res.status(200).json({
-//             _id: admin._id,
-//             name: admin.name,
-//             email: admin.email,
-//             token: generateToken(admin._id),
-//         });
-//     } else {
-//         res.status(401).json({ message: "Invalid credentials" });
-//     }
-// };
-
 export const loginAdmin = async (req, res) => {
     try {
         const { email, password } = req.body;
